c30de300d441f34b0f9ca6e3bd0e22c4b4f37133
Camera Oil <natanaeldwitya@gmail.com>
2025-05-03 22:56:59 +0700

    node code example

--- server_medis_gamma/accounts/views.py
+++ server_medis_gamma/accounts/views.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.views import LoginView
 from .forms import EmailLoginForm
+#Aku nggak tau ini buat apa tapi seharusnya kalau ada ini bakalan bisa
+# kalau nggak ada bakalan rusak
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        # Override to use email instead of username
+        request.data['username'] = request.data.get('email')
+        return super().post(request, *args, **kwargs)
+
 
 class StaffLoginView(LoginView):
     authentication_form = EmailLoginForm
--- server_medis_gamma/server_medis_gamma/urls.py
+++ server_medis_gamma/server_medis_gamma/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from accounts.views import CustomObtainAuthToken
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),  # login, register, etc.
     path('', include('main_app.urls')),           # index page, protected
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
-    path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth/', CustomObtainAuthToken.as_view(), name='api_token_auth'),
 ]
 
